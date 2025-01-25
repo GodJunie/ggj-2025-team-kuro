6e5f8ef6a8a71ef9ea015d092077288f7a7a89a9
Librarian423 <youg4231998@naver.com>
2025-01-25 13:27:15 +0900

    [feat] player controller

--- Assets/Scripts/GGJ/PlayerController.cs
+++ Assets/Scripts/GGJ/PlayerController.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace GGJ
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerController2D : MonoBehaviour
     {
-        
+        [SerializeField]
+        private float speed = 5f;
+
+        private Vector3 targetPosition;
+        private bool isAlive = true;
+        private bool isMoving = false;
+
+        void Start()
+        {
+            // 초기 목표 위치는 현재 위치
+            targetPosition = transform.position;
+        }
+
+        void Update()
+        {
+            if (!isAlive) return;
+
+            // mouse click input
+            if (Input.GetMouseButton(0))
+            {
+                SetTargetPosition();
+            }
+
+            // move
+            if (isMoving)
+            {
+                MoveToTarget();
+            }
+        }
+
+        private void SetTargetPosition()
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition = new Vector3(mousePosition.x, mousePosition.y, transform.position.z); // Y축만 변경
+            isMoving = true;
+        }
+
+        private void MoveToTarget()
+        {
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            transform.position += direction * speed * Time.deltaTime;
+
+            // Arrived at dest
+            if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+            {
+                isMoving = false;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
