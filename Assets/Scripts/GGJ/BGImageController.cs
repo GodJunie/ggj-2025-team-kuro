f05eb3de742570b320ae8f765c1c07a17890b520
Librarian423 <youg4231998@naver.com>
2025-01-26 10:13:30 +0900

    [feat] background image moving

--- Assets/Scripts/GGJ/BGImageController.cs
+++ Assets/Scripts/GGJ/BGImageController.cs
@@ -1,16 +1,81 @@
+using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class BGImageController : MonoBehaviour
+namespace GGJ
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class BGImageController : MonoBehaviour
     {
-        
+        [SerializeField] List<GameObject> bgObjects;
+
+        [SerializeField] float xDistance = 10;
+        [SerializeField] float yDistance = 17.8f;
+
+        [SerializeField] private PlayerController player;
+
+        // private void Start()
+        // {
+        //     float playerX = player.transform.position.x;
+        //     print(playerX);
+        //     foreach (var item in bgObjects)
+        //     {
+        //         print(item.name + item.transform.position+ Mathf.Abs(playerX - item.transform.position.x));
+                
+        //     }
+        // }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (player.IsDead) return;
+
+            AdjustXaxis();
+            AdjustYaxis();
+        }
+
+        private void AdjustXaxis()
+        {
+            float playerX = player.transform.position.x;
+
+            foreach (var image in bgObjects)
+            {
+                float imageX = image.transform.position.x;
+                float imageY = image.transform.position.y;
+                if (Mathf.Abs(playerX - imageX) >= (xDistance * 1.5f))
+                {
+                    print(image.name);
+                    float targetX = playerX > imageX ?
+                    imageX + xDistance * 3 //move to right
+                : imageX - xDistance * 3;  //move to left
+
+                    //image.transform.position.Set(targetX, imageY, 0f);
+                    image.transform.position = new Vector3(targetX, imageY, 0f);
+                }
+            }
+        }
+
+        private void AdjustYaxis()
+        {
+            float playerY = player.transform.position.y;
+
+            foreach (var image in bgObjects)
+            {
+                float imageX = image.transform.position.x;
+                float imageY = image.transform.position.y;
+                if (Mathf.Abs(playerY - imageY) >= (yDistance * 2))
+                {
+                    float targetY = playerY > imageY ?
+                    imageY + yDistance * 3 //move to down
+                : imageY - yDistance * 3;  //move to up
+
+                    //image.transform.position.Set(imageX, targetY, 0f);
+                    image.transform.position = new Vector3(imageX, targetY, 0f);
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
